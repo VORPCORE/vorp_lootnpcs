98a1b4b848794308d613f6e99b97d22bc2832efd
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:10:55 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,11 @@ fx_version "cerulean"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game "rdr3"
 
+author 'refactor by @outsider31000'
+lua54 'yes'
+description 'A npc looting script for vorp core framework'
+repository 'https://github.com/VORPCORE/vorp_lootnpcs'
+
 exports {
     'DataViewNativeGetEventData2'
 }
@@ -25,6 +30,7 @@ dependencies {
     'vorp_inventory'
 }
 
-author 'refactor by @outsider31000'
-lua54 'yes'
 version '1.3'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_lootnpcs'
