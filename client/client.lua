3059e7aea5bc605378909b3c53955f5ab701ca0f
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-05-07 01:36:02 +0100

    fix(client): loot peds
    removed redundant natives

--- client/client.lua
+++ client/client.lua
@@ -9,25 +9,20 @@ CreateThread(function()
         if size > 0 then
             for i = 0, size - 1 do
                 local eventAtIndex = GetEventAtIndex(0, i)
-
-                if eventAtIndex == 1376140891 then
-                    local eventStruct = DataView.ArrayBuffer(256)
-                    eventStruct:SetInt32(0, 0)
-                    eventStruct:SetInt32(8, 0)
-                    eventStruct:SetInt32(16, 0)
-                    local view = Citizen.InvokeNative(0x57EC5FA4D4D6AFCA, 0, i, eventStruct:Buffer(), 3);
-                    local entity = view[2]
-                    if not GetPedAnimalType(entity) then
-                        local eventDataSize = 3
-                        local eventDataStruct = DataView.ArrayBuffer(128)
-                        eventDataStruct:SetInt32(0, 0)
-                        eventDataStruct:SetInt32(8, 0)
-                        eventDataStruct:SetInt32(16, 0)
-                        local is_data_exists = Citizen.InvokeNative(0x57EC5FA4D4D6AFCA, 0, i, eventDataStruct:Buffer(), eventDataSize)
-                        if is_data_exists then -- can contiue
-                            if PlayerPedId() == eventDataStruct:GetInt32(0) then
-                                local type = GetPedType(entity)
-                                if type == 4 then
+                if eventAtIndex == `EVENT_LOOT_COMPLETE` then
+                    local eventDataSize = 3
+                    local eventDataStruct = DataView.ArrayBuffer(128)
+                    eventDataStruct:SetInt32(0, 0)
+                    eventDataStruct:SetInt32(8, 0)
+                    eventDataStruct:SetInt32(16, 0)
+                    local is_data_exists = Citizen.InvokeNative(0x57EC5FA4D4D6AFCA, 0, i, eventDataStruct:Buffer(), eventDataSize)
+                    if is_data_exists then -- can contiue
+                        local looter = eventDataStruct:GetInt32(0)
+                        local entity = eventDataStruct:GetInt32(8)
+                        local is_looted = eventDataStruct:GetInt32(16)
+                        if is_looted == 1 then
+                            if IsPedHuman(entity) then
+                                if PlayerPedId() == looter then
                                     if Citizen.InvokeNative(0x8DE41E9902E85756, entity) then -- _IS_ENTITY_FULLY_LOOTED
                                         local netid = NetworkGetNetworkIdFromEntity(entity)
                                         if not lootedNpcs[netid] then
